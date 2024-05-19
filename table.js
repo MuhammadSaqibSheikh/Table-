6f33951c23174d468ae626d8251de360d1eed3c5
[Saqib-Sheikh] <[saqisheikh86@gmail.com]>
2024-05-19 19:42:15 +0500

    1st commit

---
+++ table.js
@@ -0,0 +1,32 @@
+#! /usr/bin/env/ node
+import inquirer from "inquirer";
+let Mytable = true;
+while (Mytable) {
+    let myinput = await inquirer.prompt({
+        type: "number",
+        name: "Num",
+        message: "Enter your Number",
+    });
+    let Mynumber = myinput.Num;
+    // console.log(Mynumber);
+    if (Mynumber) {
+        console.log(`Here is the table of ${Mynumber} `);
+        for (let i = 1; i <= 10; i++) {
+            console.log(`${Mynumber} X ${i} = ${Mynumber * i}`);
+        }
+        ;
+    }
+    else {
+        console.log(`Enter a input in numbericsl form`);
+    }
+    let AgainAsk = await inquirer.prompt({
+        type: "confirm",
+        name: "userask",
+        message: "Do you want to another table?",
+        default: false,
+    });
+    if (AgainAsk.userask == false) {
+        Mytable = false;
+        console.log(`\n Thank you for Use my table sheet.`);
+    }
+}
